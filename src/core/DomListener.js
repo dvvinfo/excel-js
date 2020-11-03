27bf92eac4a8e5e33d989873af4d069af608f789
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-11-03 18:04:42 +0300

    Формирование верстки компонентов

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,2 +1,9 @@
-export class DomListener {}
+export class DomListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener')
+    }
+    this.$root = $root
+  }
+}
 
